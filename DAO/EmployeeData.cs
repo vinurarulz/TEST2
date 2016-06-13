af57a7888dc3dfb45b7902398d2bb118821f5f72
vinurarulz <vinurarulz@gmail.com>
2016-06-13 22:52:34 +0530

    newwww

---
+++ DAO/EmployeeData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class EmployeeData
+    {
+        public bool SaveEmployee(string fname, string lname, string tel)
+        {
+            bool result = false;
+            try
+            {
+                using (var db = new Database1Entities())
+                {
+                    var employee = new Employee() { FNAME = fname, LNAME = lname, TEL = tel };
+                    db.Employees.Add(employee);
+                    db.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
+    }
+}
